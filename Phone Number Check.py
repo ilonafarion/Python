95b1c79daebfa1f597c2d36658c232998b115c51
ilonafarion <94489509+ilonafarion@users.noreply.github.com>
2022-02-08 17:04:45 +0100

    Update Phone Number Check.py

--- Phone Number Check.py
+++ Phone Number Check.py
@@ -22,3 +22,28 @@ if isPhoneNumber(text) == True:
     print( text + ' is a phone number')
 else:
     print(text + ' is not a phone number')
+    
+# version 2:  searches for a phone number in a longer text:
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0,3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return      False
+    for i in range (4,7):
+        if not text[i].isdecimal():
+            return  False
+    if text[7] != '-':
+        return False
+    for i in range(8,12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+text = 'Please feel free to call me at 123-123-1235. Take care!'
+for i in range(len(text)):
+    isnumber = text[i:i+12]
+    if isPhoneNumber(isnumber):
+        print('phone number found: ' + isnumber)
